85d183474a439e241b69b6f4210c45c65ff19f2a
sibeesans <105978585+sibeesans@users.noreply.github.com>
2023-08-06 11:37:48 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -2,11 +2,11 @@
 dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
 biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 ###########- COLOR CODE -##############
-REPO="https://raw.githubusercontent.com/JKLOONLINE/JKLO/main/"
+REPO="https://raw.githubusercontent.com/sibeesans/JKLO/main/"
 echo -e " [INFO] Downloading Update File"
 sleep 2
 rm -rf /tmp/menu
-wget -O /tmp/menu-master.zip "${REPO}config/menu.zip" >/dev/null 2>&1
+wget -O /tmp/menu-master.zip "${REPO}config/pukimak.zip" >/dev/null 2>&1
     mkdir /tmp/menu
     7z e /tmp/menu-master.zip -o/tmp/menu/ >/dev/null 2>&1
     chmod +x /tmp/menu/*
